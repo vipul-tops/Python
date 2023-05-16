1f8540cfe236421f4cbefc7ad7025295726db83d
vipul-tops <vmmemakiya22@gmail.com>
2023-05-16 15:22:54 +0530

    merge

---
+++ task/counting.py
@@ -0,0 +1,26 @@
+#counting of character using library function in python
+
+s=input("Enter String : ")
+
+ch=0
+w=1
+sp=0
+spch=0
+n=0
+
+for i in s:
+    if i.isalpha():
+        ch=ch+1
+    elif i.isspace():
+        w=w+1
+        sp=sp+1
+    elif i.isnumeric():
+        n=n+1
+
+    else:
+        spch=spch+1
+print("Total Character : ",ch)
+print("Total Word : ",w)
+print("Total Space : ",sp)
+print("Total Special Character : ",spch)
+print("Toatal Number : ",n)
---
+++ task/divided 5  modulo 7.py
@@ -0,0 +1,7 @@
+l=[]
+
+for i in range(2000,3201):
+    if i%5==0 and i%7!=0:
+        l.append(i)
+
+print(l)
